8643fa64728561af55c86e128a93f172e261f3a5
Jérôme Verstrynge <jverstry@gmail.com>
2012-08-18 11:29:33 +0200

    Finalizing standalone hibernate JPA example

--- Standalone-Hibernate-JPA/src/main/java/com/jverstry/standalone/Item.java
+++ Standalone-Hibernate-JPA/src/main/java/com/jverstry/standalone/Item.java
@@ -3,12 +3,18 @@ package com.jverstry.standalone;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.AutoProperty;
 
 @Entity
+@AutoProperty
 public class Item implements Serializable {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long ID;
 	
 	private String name = "";
@@ -23,6 +29,21 @@ public class Item implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		return Pojomatic.equals(this, o);
+	}
+
+	@Override
+	public int hashCode() {
+		return Pojomatic.hashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return Pojomatic.toString(this);
 	}
 	
 }
---
+++ Standalone-Hibernate-JPA/src/main/java/com/jverstry/standalone/StandaloneHibernateJPAExample.java
@@ -0,0 +1,83 @@
+
+package com.jverstry.standalone;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class StandaloneHibernateJPAExample {
+	
+	private static EntityManagerFactory EMF;
+	private static EntityManager EM;
+	
+	public static void main(String[] args) {
+
+		// Creating resources
+		EMF = Persistence.createEntityManagerFactory("Standalone");
+		EM = EMF.createEntityManager();
+
+		// Create
+		Item i = new Item();
+		i.setName("Item A");
+		System.out.println("Before saving   : " + i);
+		save(i);
+		System.out.println("After saving    : " + i);
+		
+		// Read
+		Item retr = get(Item.class, i.getID());
+		System.out.println("Retrieved I     : " + retr);
+
+		// Update
+		i.setName("Item B");
+		System.out.println("Updated         : " + i);
+		update(i);
+		retr = get(Item.class, i.getID());
+		System.out.println("Retrieved II    : " + retr);
+		
+		// Delete
+		System.out.println("Deleting        : " + i);
+		delete(i);
+		retr = get(Item.class, i.getID());
+		System.out.println("Retrieved III   : " + retr);
+
+		// Closing resources
+		EM.close();
+		EMF.close();
+		
+	}
+	
+	public static void save(Object o) {
+		
+		EntityTransaction et = EM.getTransaction();
+		et.begin();
+		EM.persist(o);
+		et.commit();
+		
+	}
+	
+	public static <T> T get(Class<T> entityType, Object key) {
+		
+		return EM.find(entityType, key);
+		
+	}
+	
+	public static void update(Object o) {
+		
+		EntityTransaction et = EM.getTransaction();
+		et.begin();
+		EM.merge(o);
+		et.commit();
+		
+	}
+	
+	public static void delete(Object o) {
+		
+		EntityTransaction et = EM.getTransaction();
+		et.begin();
+		EM.remove(o);
+		et.commit();
+		
+	}
+	
+}
