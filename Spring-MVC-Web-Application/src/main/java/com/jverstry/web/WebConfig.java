60ea596d1cf183ad3885c15e3fa1936257bcaf04
JVerstry <jverstry@gmail.com>
2012-08-27 12:47:52 +0200

    Spring web application example finalization

--- Spring-MVC-Web-Application/src/main/java/com/jverstry/web/WebConfig.java
+++ Spring-MVC-Web-Application/src/main/java/com/jverstry/web/WebConfig.java
@@ -1,19 +1,43 @@
 
-package com.firmflows.web;
+package com.jverstry.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
+/**
+ * Replaces the typical application-context.xml file.
+ */
 @EnableWebMvc
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 	
 	/**
+	 * A view resolver takes the value returned by methods handling
+	 * user requests and typically converts it into a file/resource location.
+	 */
+	@Bean
+	public ViewResolver getViewResolver() {
+		
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		// This passes the view (JSP) for taglibs processin (JSLT)
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/pages/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+		
+	}
+	
+	/**
 	 * Configuration for a request handler for serving static resources.
 	 * 
-	 * This is to be used when the Spring MVC DispatcherServlet is
+	 * This is indended to be used when the Spring MVC DispatcherServlet is
 	 * mapped to "/" thus overriding the Servlet container's default handling
 	 * of static resources.
 	 * 
