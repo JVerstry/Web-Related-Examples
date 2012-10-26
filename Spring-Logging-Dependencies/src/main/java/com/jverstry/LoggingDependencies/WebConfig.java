9b93730cb9c761b2fab010513eb19314654b93c9
JVerstry <jverstry@gmail.com>
2012-10-26 11:28:15 +0200

    Small update/clean-up

--- Spring-Logging-Dependencies/src/main/java/com/jverstry/LoggingDependencies/WebConfig.java
+++ Spring-Logging-Dependencies/src/main/java/com/jverstry/LoggingDependencies/WebConfig.java
@@ -4,7 +4,6 @@ package com.jverstry.LoggingDependencies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
