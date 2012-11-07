bfd2babf29d50aa82cfb47e2c78634d2fbd1e00f
JVerstry <jverstry@gmail.com>
2012-11-07 12:11:01 +0100

    Correcting small glitch in Javascript

--- Spring-MVC-Error-Handling/src/main/webapp/resources/js/SpringMVCErrorHandling.js
+++ Spring-MVC-Error-Handling/src/main/webapp/resources/js/SpringMVCErrorHandling.js
@@ -6,7 +6,7 @@ var ExceptionHandler = function() {
     $.ajax({
         type: 'GET',
         url:  prefix + "/throwCustomized4ExceptionHandler",
-		
+        dataType: 'json',
         async: true,
         success: function(result) {
 			alert('Unexpected success !!!');
